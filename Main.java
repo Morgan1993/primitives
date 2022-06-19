86dd1d967485c19e6957b3947c31b183540c35a8
Евгений Моргунов <morgunov-zhenya@mail.ru>
2022-06-19 19:14:41 +0300

    Initial

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int oneMilePrice = 20;
+        int milesAmount = ticketPrice / oneMilePrice;
+        System.out.println("Количество бонусных миль " + milesAmount);
+    }
+}
